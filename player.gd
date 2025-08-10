be7b2ba740ff9039b965d21649dfe514837e9521
Evan Boerchers <eboerchers@yahoo.ca>
2025-08-09 20:45:36 -0700

    spaceship movement

--- player.gd
+++ player.gd
@@ -1,37 +1,52 @@
-extends Area2D
+extends RigidBody2D
 
 signal hit
 
-@export var speed = 400
-var screen_size
+@export var thrust      := 1200.0
+@export var turn_torque := 9000.0 
+@export var max_speed   := 900.0  
+@export var lin_damp    := 2.0    
+@export var ang_damp    := 2      
 
-func start(pos):
+var screen_size: Vector2
+
+func start(pos: Vector2) -> void:
 	position = pos
 	show()
-	$CollisionPolygon2D.disabled = false
+	$Hitbox.disabled = false
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	linear_damp = lin_damp
+	angular_damp = ang_damp
+	$Thrust.visible = false
 
-func _process(delta):
-	var velocity = Vector2.ZERO
+func _physics_process(_delta: float) -> void:
+	var rot_dir := 0.0
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		rot_dir += 1.0
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		rot_dir -= 1.0
+	apply_torque( rot_dir * turn_torque) 
+
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		var forward := Vector2.UP.rotated(rotation)
+		apply_force(forward * thrust) 
+		$Thrust.visible = true
+	else:
+		$Thrust.visible = false
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)		
+	var speed := linear_velocity.length()
+	if speed > max_speed:
+		linear_velocity = linear_velocity.normalized() * max_speed
 
+	_wrap_to_screen()
 
-func _on_body_entered(_body):
+func _wrap_to_screen() -> void:
+	position.x = wrapf(position.x, 0, screen_size.x)
+	position.y = wrapf(position.y, 0, screen_size.y)
+
+func _on_body_entered(_body: Node) -> void:
 	hide()
 	hit.emit()
-	$CollisionPolygon2D.set_deferred("disabled", true)
+	$Hitbox.set_deferred("disabled", true)
