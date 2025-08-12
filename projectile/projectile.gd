92f5bfa9bb7976106418ae46044f19083effb3ed
evan.boerchers@gamingrealms.com <evan@blastworksinc.com>
2025-08-12 15:23:54 -0700

    corrected masking

--- projectile/projectile.gd
+++ projectile/projectile.gd
@@ -4,3 +4,7 @@ extends Area2D
 
 func _physics_process(delta: float) -> void:
 	position += transform.x * speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
