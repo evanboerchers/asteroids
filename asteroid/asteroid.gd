3ea3ec3cf190efa0ae21c27dece6985dc3640f30
Evan Boerchers <eboerchers@yahoo.ca>
2025-08-12 22:10:48 -0700

    work

--- asteroid/asteroid.gd
+++ asteroid/asteroid.gd
@@ -4,11 +4,8 @@ func _ready() -> void:
 	add_to_group('asteroid')
 
 func hit() -> void:
-	queue_free()
+	call_deferred("queue_free")
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+	call_deferred("queue_free")
 
-func _on_body_entered(body: Node) -> void:
-	hit()
-	body.queue_free()
